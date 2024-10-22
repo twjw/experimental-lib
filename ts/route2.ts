07f86ceb16009338bf9ca69deb05fd174ae18437
twjw <aaaawjwa@gmail.com>
2024-10-22 18:41:52 +0800

    結果描述

---
+++ ts/route2.ts
@@ -0,0 +1,21 @@
+/*
+(layout) // 要跳脫
+  about
+    page.tsx
+  layout.tsx // 裡面要自己放 <Outlet />
+  page.tsx
+login
+  page.tsx
+
+↑ / > (layout)/page.tsx
+  /about > (layout)/about/page.tsx
+  /login > login/page.tsx
+
+  <Routes>
+    <Route path="/login" element={<Page />} />
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Page />} />
+      <Route path="about" element={<Page />} />
+    </Route>
+  </Routes>
+*/
